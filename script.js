a405a82a8c00dc608d2a3bf80cfa4f1d58832578
sanket-walanj <code.sanky@gmail.com>
2022-04-03 20:27:40 +0530

    final

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
+const word = document.querySelector('.input-text');
 const btn = document.querySelector('.btn');
 const result = document.querySelector('.result');
 
-btn.addEventListener('click', palindrome);
+btn.addEventListener('click', countVowel);
 
-function palindrome() {
-  const word = document.querySelector('.input-text').value;
-  let len = word.length;
+function countVowel() {
+  let vowelCount = 0;
+  let wordVal = word.value.toLowerCase();
 
-  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
-  // alert(start);
-  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
-  // alert(end);
-  // let flip = end.split('').reverse().join('');
-  let flip = [...end].reverse().join('');
-  // alert(flip);
-  if (start === flip) {
-    result.innerHTML = `${word.toUpperCase()} is a Palindrome`;
-  } else {
-    result.innerHTML = `${word.toUpperCase()} is not a Palindrome`;
+  for (let i = 0; i < wordVal.length; i++) {
+    let letter = wordVal[i];
+    if (letter.match(/([a,e,i,o,u])/)) {
+      vowelCount++;
+    }
   }
+  result.innerHTML = `${word.value.toUpperCase()} has ${vowelCount} vowels `;
 }
